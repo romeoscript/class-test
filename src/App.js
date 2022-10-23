896255f71cda28305be3b4ae2c320ec474fd7d8f
Romeoscript <romeobourne211@gmail.com>
2022-10-23 05:51:48 +0100

    router done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Home, About, Login, Register } from "./pages";
 // import Files from "./component/files/Files";
 
 const App = () => {
-
-
-
   return (
     <div className="App">
-  
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import About from "./about/About";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import Register from "./register/Register";
+
+export {
+    About,
+    Home,
+    Login,
+    Register
+}
