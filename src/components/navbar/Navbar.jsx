1a419f34b493edf10779fdd6ccf5d7f76353d2ac
Romeoscript <romeobourne211@gmail.com>
2022-10-28 14:59:20 +0100

    navbar fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,62 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
 const Navbar = () => {
-  const [bgChange, setBgChange] = useState(false)
+  const [bgChange, setBgChange] = useState(false);
+  const [navToggle, setNavToggle] = useState(true);
 
-  const changeBg =()=>{
+  // changenavbg
+  const changeBg = () => {
     window.scrollY >= 80 ? setBgChange(true) : setBgChange(false);
-  }
-  window.addEventListener('scroll', changeBg)
+  };
+  window.addEventListener("scroll", changeBg);
+
+  // togglemobile
+  const toggleMobile = () => {
+    setNavToggle(!navToggle);
+  };
+
   return (
-    <div className='navbar__container active' >
-      <div className={bgChange?'navlinks__container active':'navlinks__container'}>
-        <p>Trade</p>
-        <p>About us</p>
-        <p>Log in</p>
-        <p>Sign up</p>
+    <div className="navbar__container active">
+      <div
+        className={
+          bgChange ? "navlinks__container active" : "navlinks__container"
+        }
+      >
+        <Link to="/">
+          <p>Trade</p>
+        </Link>
+        <Link to="./about">
+          <p>About us</p>
+        </Link>
+        <Link to="/login">
+          <p>Log in</p>
+        </Link>
+        <Link to="/register">
+          <p>Sign up</p>
+        </Link>
       </div>
+
+      {navToggle && <HiMenuAlt3 className="menu" onClick={toggleMobile} />}
+      {!navToggle && <MdClose className="menu" onClick={toggleMobile} />}
+      {!navToggle && (
+        <div className="mobile__view">
+          <Link to="/">
+            <p>Trade</p>
+          </Link>
+          <Link to="./about">
+            <p>About us</p>
+          </Link>
+          <Link to="/login">
+            <p>Log in</p>
+          </Link>
+          <Link to="/register">
+            <p>Sign up</p>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
