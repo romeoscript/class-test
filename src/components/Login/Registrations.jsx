3ee92344d157a64c274d5c554b201b58ee5dbfc4
Romeoscript <romeobourne211@gmail.com>
2022-10-30 22:14:33 +0100

    login and registration page done and dusted

---
+++ src/components/Login/Registrations.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./logins.scss";
+const Registrations = () => {
+  return (
+    <div className="logins loginss">
+      <form action="#">
+        <section className="name__section">
+          <div>
+            {" "}
+            <label htmlFor="name">Surname</label> <br />
+            <input type="text" />
+          </div>
+          <div>
+            {" "}
+            <label htmlFor="name">FirstName</label> <br />
+            <input type="text" />
+          </div>
+        </section>
+        <label htmlFor="password">password</label> <br />
+        <input type="password" />
+        <section className="name__section">
+          <div>
+            {" "}
+            <label htmlFor="name">Password</label> <br />
+            <input type="password" />
+          </div>
+          <div>
+            {" "}
+            <label htmlFor="name">Confirm Password</label> <br />
+            <input type="password" />
+          </div>
+        </section>
+        <section className="choose__pin">
+          <p>choose pin</p>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </section>
+        <input type="submit" value="Sign in" />
+        <p>already have an account? login</p>
+      </form>
+    </div>
+  );
+};
+
+export default Registrations;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Registrations from '../../components/Login/Registrations';
+import Wrapper from "../../components/wrapper/Wrapper";
+import '../login/login.scss'
+import './register.scss'
 
 const Register = () => {
   return (
     <div>
-      register
-    </div>
+    <Wrapper>
+      {/* <Navbar /> */}
+      <div className="auth__wrapper">
+        <div className="auth__wrapper-text register__wrapper">
+          <h2>Buy and Sell Foreign Currency</h2>
+        </div>
+        <Registrations />
+      </div>
+    </Wrapper>
+  </div>
   )
 }
 
